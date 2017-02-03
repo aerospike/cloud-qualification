25118d1023a92c668e3c4d7f53034470bc017736
Richard Guo <r.guo@aerospike.com>
2017-02-03 14:32:19 -0800

    Initial AWS checkin

--- README.md
+++ README.md
@@ -1,2 +1,163 @@
-# cloud-certification
-Scripts and instructions to run benchmarks/tests on cloud environments.
+# Cloud Certification
+
+This project contains scripts that will:
+
+1. Create an Aerospike cluster
+2. Create client systems
+3. Run YCSB from client systems against the Aerospike cluster
+4. Change Aerospike parameters and reset the cluster
+5. Repeat 3 and 4 until all parameters are exhausted
+
+With 100M objects, this process takes ~1 hour per YCSB test.
+
+This project can be utilized in several ways:
+
+1. Running a simple YCSB test with a pre-defined aerospike config
+2. Finding the limits of a particular instance/environment
+3. Finding the impact of Aerospike configuration changes
+
+## Requirements
+
+* Boto3 for Python `sudo pip install --upgrade boto3`
+  * AWS credentials pre-configured at ~/.aws/credentials
+* certificate based login to all systems
+
+## Parameters
+Parameters are in the following dimensions:
+
+### 1. Server Parameters
+Server side parameters such as write-block-size and service-threads.  
+The default configs are held in `aerospike.conf`. A parameter *needs* to be defined in `aerospike.conf` in order for the Tests section of `params.yaml` to be applied. Otherwise the YCSB test will just run against your default config.
+See the Tests section of `params.yaml`
+
+### 2. Instance Parameters
+#### EC2 ####
+Aerospike instance size as in the cluster size, and instance type  
+See the Servers section of `params.yaml` 
+
+YCSB Client instance size as in number of clients and instance type.
+See the Clients section of `params.yaml`
+
+**Notes** It is highly suggested to use spot pricing. Take a moment to check out spot pricing history and be flexible on which AZ to utilize to achieve maximum cost savings.
+
+### 3. Workload
+
+The workload is a YCSB workload config file named `workload-aerospike`. Edit this file to change how YCSB runs.
+
+The workload defines:
+
+ - The read/update ratio. Update is equivalent to 1 read and 1 write.
+ - The access pattern (uniform, zipfian or latest)
+ - Size of record
+ - Number of records
+ - Runtime
+
+## Results
+All results will be found in the `data/` directory. Each file will be a yaml dump of the results. The filenames are based on the Server side Test ran.
+
+
+# Cloud Qualification
+
+The Cloud Qualification is as follows:
+
+The highest transactions/s (X) that can be accomplished while maintaining average read latency under 1ms.
+
+With X being a workload of 50/50 read/updates, of uniform access pattern.
+
+**Data Preload**
+
+The data should be preloaded to be near 50% of disk fnd 60% of memory for worst-case scenario. Object size would vary depending on ratio of memory to disk, to suit the instance type. This would be the limit for a 2 node cluster (due to replication factor of 2). Then add another node. The resulting 3 node cluster would form the basis for this test.
+
+To calculate record size and number of objects:
+
+* Disk used = (Record Size + 128) * Number of Objects
+* Memory used = 64 * Number of Objects
+
+Data preload is done using YCSB's load phase. 
+
+**Running the Cloud Qualification**
+
+_AWS_
+
+The default settings are already configured for qualifying on instances with 30GB of ram.
+
+Edit `workload-aerospike` with the number of objects and object size as determined from above. `recordcount` and `operationcount` should match. Make sure that `maxexecutiontime` is long enough to run the entire test.
+
+Edit `params.yaml` such that the `Tests` section contains only the following:
+
+```
+write-block-size: 1024,1024,2
+```
+This will result in only a single pass of testing, and all Aerospike parameters are kept at default (write block size at 1M)
+
+The `-l` parameter for `run_benchmark.py` is used to set the target ops/s.
+
+## Usage:
+
+Create an environment using the respective `create_*_stack` script.
+
+Once the environment is up, use run\_bench.py to run the actual benchmarks.
+
+```
+usage: run_bench.py [-h] [-v] -c CONFIG -n NAMESPACE [-d [DEPLOYMENT]]
+                    [-p [PROJECT]] [-t [TEMPLATE]] [-l [LOAD]] [-z [THREADS]]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v, --verbose         Enable verbose logging
+  -c CONFIG, --config CONFIG
+                        The config file to use
+  -n NAMESPACE, --namespace NAMESPACE
+                        The namespace to bench against
+  -d [DEPLOYMENT], --deployment [DEPLOYMENT]
+                        The GCE deployment
+  -p [PROJECT], --project [PROJECT]
+                        The GCE project
+  -t [TEMPLATE], --template [TEMPLATE]
+                        The CFT for aerospike server
+  -l [LOAD], --load [LOAD]
+                        The target ops/s for YCSB
+  -z [THREADS], --threads [THREADS]
+                        The thread count for YCSB
+```
+
+## TODO
+
+* GCP versions
+
+
+## Files
+
+* cft - Directory of EC2 **C**loud**F**ormation **T**emplates. Used in spinning up an EC2 stack
+* scripts - Directory of scripts that may be used on the Aerospike Servers themselves for additional configurations
+* README.md - This README file
+* aerospike.conf - The default Aerospike configuration
+* aerospike.jinja - GCP template for creating Aerospike instances
+* aerospike.jinja.schema - GCP template for parameter validation
+* config.yaml - GCP config file 
+* create\_ec2\_stack - script to create an EC2 stack
+* params.yaml - The main config file for this project
+* requirements.txt - Python requirements file
+* run\_bench.py - The main script that runs the YCSB tests
+* workload-aerospike - The YCSB workload file that's run by YCSB for the benchmarking.
+
+```
+├── aerospike.conf
+├── cft
+│   ├── aerospike.json
+│   └── clients.json
+├── config.yaml
+├── create_ec2_stack
+├── create_graphs.py
+├── data
+├── params.yaml.template
+├── README.md
+├── requirements.txt
+├── run_bench.py
+├── scripts
+│   ├── aerospike_image
+│   ├── balance_scsi_mq
+│   ├── partition_disk
+│   └── testing.cron
+└── workload-aerospike
+```
---
+++ run_bench.py
@@ -0,0 +1,457 @@
+#!/usr/bin/env python
+
+import argparse
+import boto3
+import botocore
+import yaml
+import socket
+import csv
+import re
+import sys
+import time
+import subprocess
+from multiprocessing.pool import ThreadPool
+#from googleapiclient import discovery
+from threading import Thread
+from pprint import pprint
+#from oauth2client.client import GoogleCredentials
+#credentials = GoogleCredentials.get_application_default()
+#compute = discovery.build('compute','v1',credentials=credentials)
+#service = discovery.build('deploymentmanager','v2',credentials=credentials)
+
+args = None
+data = {}
+DATA_FILES="data"
+asd_version = ""
+user="ec2-user"
+pool = ThreadPool(processes=2)
+private_ips = None
+
+
+try:
+  import boto3
+except:
+  print "Boto3 is not installed. Please install boto3: sudo pip install boto3"
+  exit
+
+
+def parse_args():
+    global args
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("-v"
+                        , "--verbose"
+                        , action="store_true"
+                        , dest="debug"
+                        , help="Enable verbose logging")
+
+    parser.add_argument("-c"
+                        , "--config"
+                        , dest="config"
+                        , required=True
+                        , help="The config file to use")
+    parser.add_argument("-n"
+                        , "--namespace"
+                        , dest="namespace"
+                        , required=True
+                        , help="The namespace to bench against")
+    parser.add_argument("-d"
+                        , "--deployment"
+                        , dest="deployment"
+                        , default='aerospike-test'
+                        , nargs='?'
+                        , help="The GCE deployment")
+    parser.add_argument("-p"
+                        , "--project"
+                        , dest="project"
+                        , default='aerospike-dev'
+                        , nargs='?'
+                        , help="The GCE project")
+    parser.add_argument("-t"
+                        , "--template"
+                        , dest="template"
+                        , default='cft/aerospike.json'
+                        , nargs='?'
+                        , help="The CFT for aerospike server")
+    parser.add_argument("-o"
+                        , "--ops"
+                        , dest="ops"
+                        , default=50000
+                        , nargs='?'
+                        , help="The target ops/s for YCSB")
+    parser.add_argument("-z"
+                        , "--threads"
+                        , dest="threads"
+                        , default=10
+                        , nargs='?'
+                        , help="The thread count for YCSB")
+    parser.add_argument("-l"
+                        , "--load"
+                        , dest="load"
+                        , action='store_true'
+                        , help="Run the Loading phase (Inserts)")
+    parser.add_argument("-r"
+                        , "--run"
+                        , dest="run"
+                        , action='store_true'
+                        , help="Run the Running phase (Read/Update)")
+
+    args = parser.parse_args()
+    
+   
+#----
+# Compile YCSB
+# Not actually needed
+#---
+def compileYCSB(ip, config):
+    print "Compiling YCSB"
+    ssh_command(ip,config, "source %s; cd YCSB; mvn -pl com.yahoo.ycsb:aerospike-binding -am clean package"%(profile))
+    
+#----
+# Write a data file
+#----
+def write_datafile(filename,yaml_data):
+    with open(DATA_FILES+'/'+filename, 'w') as outfile:
+        yaml.dump(yaml_data,outfile, default_flow_style=True)
+
+def add_data(metric, param, yaml_data,iteration):
+    global data
+    if iteration not in data:
+        data[iteration] = {}
+    key = "%s-%d"%(metric,param)
+    if key not in data[iteration]:
+        data[iteration][key] = yaml_data
+
+def write_yaml_to_csv(yaml_data,config,version):
+    filename = "%s_%s_%s.csv"%(config['Servers']['InstanceType'], config['Servers']['NumberOfInstances'], version)
+    f = open(DATA_FILES+"/"+filename, 'wt')
+    try:
+        writer = csv.writer(f)
+        i = 0
+        for thread in yaml_data:
+            for metric in sorted(yaml_data[thread]):
+                param,value = re.split('-(?=\d*$)', metric)
+                data_line = (value, )
+                header_line = (param, )
+                for operation in yaml_data[thread][metric]:
+                    for stat in yaml_data[thread][metric][operation]:   
+                        data_line += (yaml_data[thread][metric][operation][stat],)
+                        header_line += (operation+' '+stat,) 
+                if i %4 == 0:
+                    writer.writerow(header_line)
+                writer.writerow(data_line)   
+                i+=1
+    finally:
+        f.close()
+
+
+#-------
+# Have client run a benchmark
+#-------
+def load_benchmark((ip,server,config)):
+    print "Running insert benchmark"
+    stdout= ssh_command(ip,config,"source %s; cd YCSB; bin/ycsb load aerospike -s -threads %s -target %s -P workloads/workload-aerospike -p as.host=%s -p as.namespace=%s 2> load.log"%(profile,args.threads,args.ops,server,args.namespace))
+    if args.debug:
+        pprint(stdout)
+    return stdout
+
+def run_benchmark((ip,server,config)):
+    print "Running read/update benchmark"
+    stdout= ssh_command(ip,config, "source %s; cd YCSB; bin/ycsb run aerospike -s -threads %s -target %s -P workloads/workload-aerospike -p as.host=%s -p as.namespace=%s 2> run.log"%(profile,args.threads,args.ops,server,args.namespace))
+    if args.debug:
+        pprint(stdout)
+    return stdout
+
+    
+def parse_output(ycsb_output):
+    ycsb_dict = {}
+    for line in ycsb_output.split("\r\n"):
+        if line == '':
+            continue
+        pprint(line)
+        items = line.split(',')
+        category = items[0].strip('[]')
+        if category not in ycsb_dict:
+            ycsb_dict[category] = {}
+        ycsb_dict[category][items[1]] = items[2]
+    return ycsb_dict
+
+#--------
+# Run the tests found in the Tests section of the config
+#-------
+def run_tests(client_ips,server,config,iteration):
+    global asd_version, private_ips  
+    asd_version  = get_asd_version(server[0],config).replace(' ','_').strip()
+    for test,values in config['Tests'].iteritems():
+        start, end, interval = map(int, values.split(','))
+        while (start <= end):
+            params = []
+            for ip in client_ips:
+              params.append((ip,private_ips[0],config))
+            if  not args.run or (args.run and args.load):
+                stop_server(server,config)
+                reset_server_ssd(server,config)
+                if "write-block-size" == test:
+                    update_server_config(server,test,"%dk"%start,config)
+                else:
+                    update_server_config(server,test,start,config)
+                restart_server(server,config)
+                results = pool.map(load_benchmark,params)
+                i=0
+                for result in results:
+                  add_data(test+"_load",start,parse_output(result),i)
+                  i+=1
+#            pprint(data)
+            if not args.load or (args.run and args.load):
+                run_results = pool.map(run_benchmark,params)
+                i=0
+                for result in run_results:
+                    add_data(test,start,parse_output(result),i)
+                    i+=1
+
+            if "write-block-size" == test:
+                start *= interval
+            else:
+                start += interval
+
+#---
+# SSH command
+#---
+def ssh_command(ip,config,command):
+    ssh_cmd = "ssh"
+    if not args.debug:
+        ssh_cmd += " -o LogLevel=QUIET "
+    ssh_cmd += " -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -t -i "+config['PKey']+" "+user+"@"+ip
+    sys_cmd = ssh_cmd +' "'+ command+'"'
+    proc = subprocess.Popen(sys_cmd, shell=True, stdout=subprocess.PIPE)
+    sys.stdout.write("\n")
+    return proc.stdout.read()
+
+#----
+# Reconfigure the server
+#-----
+def get_asd_version(server,config):
+    print "Obtaining Server Version"
+    return ssh_command(server,config, "asd --version")
+
+def reset_config(servers,config):
+    print "Resetting config to default"
+    for server in servers:
+      ssh_command(server,config, "sudo cp /etc/aerospike/aerospike.bak /etc/aerospike/aerospike.conf")
+
+def backup_config(servers,config):
+    print "Backing up default config"
+    for server in servers:
+      ssh_command(server,config, "sudo cp /etc/aerospike/aerospike.conf /etc/aerospike/aerospike.bak")
+
+def update_server_config(servers, metric, value, config):
+    print "Reconfiguring server metric: %s with value %s"%(metric, value)
+    command = "sed 's/%s.*/%s %s/' /etc/aerospike/aerospike.orig | sudo tee /etc/aerospike/aerospike.conf"%(metric,metric,value)
+    if not args.debug:
+        command += " > /dev/null"
+    for server in servers:
+      ssh_command( server, config, command)
+
+def reset_server_ssd(servers, config):
+    print "Wiping SSD at /dev/sd[cdef]"
+    for server in servers:
+     # ssh_command(server,config, "sudo dd if=/dev/zero bs=1M count=1 of=/dev/sdb1")
+     # ssh_command(server,config, "sudo dd if=/dev/zero bs=1M count=1 of=/dev/sdb2")
+     # ssh_command(server,config, "sudo dd if=/dev/zero bs=1M count=1 of=/dev/sdc1")
+     # ssh_command(server,config, "sudo dd if=/dev/zero bs=1M count=1 of=/dev/sdc2")
+      ssh_command(server,config, "sudo dd if=/dev/zero bs=1M count=1 of=/dev/sdb")
+#      ssh_command(server,config, "sudo dd if=/dev/zero bs=1M count=1 of=/dev/sdc")
+
+def stop_server(servers,config):
+    print "Stopping Aerospike"
+    for server in servers:
+      ssh_command(server,config, "sudo service aerospike stop")
+
+def restart_server(servers,config):
+    print "Restarting Aerospike"
+    for server in servers:
+      ssh_command(server,config, "sudo service aerospike restart")
+    print "Waiting for clustering"
+    time.sleep(30)
+    print "Checking for migrations on %s"%(servers[0])
+    while is_migrating(servers[0]):
+      time.sleep(10)
+      print '.',
+      sys.stdout.flush()
+    print "Migrations finished"
+
+
+def is_migrating(server):
+    response = ""
+    migrate_progress_send = 0
+    migrate_progress_recv = 0
+    partitions_remaining = 0
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect((server,3003))
+    except:
+        return True
+    s.send("statistics\n")
+    response = readall(s)
+#    print response
+    s.close()
+    if response:
+        pairs = response.split(';')
+        for pair in pairs:
+            try:
+                k, v = pair.split('=', 2)
+                if k == 'migrate_progress_send':
+                    migrate_progress_send = int(v)
+                if k == 'migrate_progress_recv':
+                    migrate_progress_recv = int(v)
+                if k == 'migrate_partitions_remaining':
+                    partitions_remaining = int(v)
+            except:
+                continue
+    else:
+        print "No response received"
+#    print "migrate_progress_send %s"%migrate_progress_send
+#    print "migrate_progress_recv %s"%migrate_progress_recv
+#    print "paritions_remaining %s"%partitions_remaining
+    if migrate_progress_send == 0 and migrate_progress_recv == 0 and partitions_remaining == 0:
+        return False
+    else:
+        return True
+
+def readall(s):
+    chunks = []
+    while True:
+        chunk = s.recv(1024 * 64)
+        if not chunk:
+            break
+        chunks.append(chunk)
+        if chunk[len(chunk)-1] == '\n':
+            break
+    if len(chunks) > 0:
+        return ''.join(chunks)
+    return None
+
+
+#   EEEE CCCC 2222
+#   E    C       2
+#   EEE  C    2222
+#   E    C    2
+#   EEEE CCCC 2222
+
+#-----
+# Extract AutoScalingGroup from CF stack
+#-----
+def extract_autoscaling_group(cf_client,stack_id):
+    stack = cf_client.list_stack_resources(StackName=stack_id)['StackResourceSummaries']
+    for resource in stack:
+        if resource['ResourceType'] == 'AWS::AutoScaling::AutoScalingGroup':
+            return resource['PhysicalResourceId']
+
+#----
+# Extract instance Ids from AutoScalingGroup
+#----
+
+def extract_instance_ids(autoscaling_client,autoscaling_group):
+    d = autoscaling_client.describe_auto_scaling_groups(AutoScalingGroupNames=[autoscaling_group])
+    return d['AutoScalingGroups'][0]['Instances']
+
+
+#-----
+# Extract instance IP from instance list
+#----
+
+def extract_instance_ip(ec2_client,instances,public=True):
+  ips = []
+  for instance in instances:
+    d = ec2_client.Instance(instance['InstanceId'])
+    if public:
+        ips.append(d.public_ip_address)
+    else:
+        ips.append(d.private_ip_address)
+  return ips
+
+
+#   GGG  CCC EEEE
+#  G    C    E
+#  G GG C    EEE
+#  G  G C    E
+#   GGG  CCC EEEE
+
+#------
+# Get server IP
+#------
+def extract_gce_ips(project,zone,instances):
+    publicIP=[]
+    for name in instances:
+        result = compute.instances().get(instance=name,zone=zone,project=project).execute()
+        publicIP.append(result['networkInterfaces'][0]['accessConfigs'][0]['natIP'])
+    return publicIP
+
+def extract_gce_instances(project,deployment):
+    instances=[]
+    result = service.resources().list(project=project,deployment=deployment).execute()
+    for resource in result['resources']:
+        if 'compute.v1.instance' == resource['type']:
+            instances.append(resource['name'])
+    return instances
+
+#-----
+# The parallelized workload
+#----
+def run_main(iteration):
+    global private_ips
+    if "EC2" == config['Platform']:
+        print "Connecting to Cloudformation at %s"%(config['Region'])
+        client = boto3.client('cloudformation',region_name=config['Region'])
+        autoscale_client = boto3.client('autoscaling',region_name=config['Region'])
+        ec2_resource = boto3.resource('ec2',region_name=config['Region'])
+        group = extract_autoscaling_group(client,config['DCNames'])
+        client_group = extract_autoscaling_group(client,config['DCNames']+'-clients')
+    if "GCP" == config['Platform']:
+        group = extract_gce_instances(args.project,args.deployment)
+    if args.debug:
+        pprint(group)
+    if group is None:
+        print "Instances not found. Aborting"
+        exit(1)
+    if "EC2" == config['Platform']:
+        instances = extract_instance_ids(autoscale_client,group)
+        client_instances = extract_instance_ids(autoscale_client,client_group)
+        server_ips = extract_instance_ip(ec2_resource,instances,public=True)
+        client_ips = extract_instance_ip(ec2_resource,client_instances)
+        private_ips = extract_instance_ip(ec2_resource,instances,public=False)
+    if "GCP" == config['Platform']:
+        server_ips = extract_gce_ips(args.project,"us-central1-f",group)
+        client_ips = extract_gce_ips(args.project,"us-central1-f",["bench-client"])   
+    
+    run_tests(client_ips,server_ips,config, iteration)
+
+##################
+# begin main part
+##################
+
+parse_args()
+
+if args.debug:
+    print "Reading params.yml file"
+
+# Read in the params file
+with open(args.config,'r') as stream:
+    try:
+        config = yaml.load(stream)
+    except yaml.YAMLError as e:
+        print(e)
+        exit(1)
+
+profile=".bash_profile"    # GCP: .profile, EC2: .bash_profile
+if "GCP" ==  config['Platform']:
+    profile=".profile"
+
+if args.debug:
+    pprint(config)
+
+run_main(0)
+
+write_datafile("Results_%s_%s_%s.yaml"%(config['Servers']['NumberOfInstances'],config['Servers']['InstanceType'],asd_version),data)
+write_yaml_to_csv(data,config,asd_version)
+
